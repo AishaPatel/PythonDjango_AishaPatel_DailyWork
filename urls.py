3738aa1f4876f419c66b24cf83a2be14c3c0682e
AishaPatel <76744703+AishaPatel@users.noreply.github.com>
2021-06-08 17:58:11 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -5,4 +5,6 @@ urlpatterns= [
     path('',views.homepageview,name="home"),
     path('about',views.aboutpageview,name="about"),
     path('contact',views.contactpageview,name="contact"),
+    path('form',views.myform,name="myform"),
+    path('formprocess',views.process,name="process")
 ]
--- views.py
+++ views.py
@@ -10,3 +10,16 @@ def aboutpageview(request):
 def contactpageview(request):
     return render(request,'contact.html')
 
+def myform(request):
+    return render(request,'myform.html')
+
+def process(request):
+    print("Welcome")
+    print(request.method)
+    print(request.POST)
+    a = int(request.POST['txt1'])
+    b = int(request.POST['txt2'])
+    c = a + b
+    print(c)
+    return render(request,'ans.html',{'mya':a,'myb':b,'mysum':c})
+
