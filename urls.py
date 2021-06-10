d1d66bf563a204f3edbbf8101fe920f3458658ee
AishaPatel <76744703+AishaPatel@users.noreply.github.com>
2021-06-11 00:14:09 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -6,5 +6,9 @@ urlpatterns= [
     path('about',views.aboutpageview,name="about"),
     path('contact',views.contactpageview,name="contact"),
     path('form',views.myform,name="myform"),
-    path('formprocess',views.process,name="process")
+    path('formprocess',views.process,name="process"),
+    path('signup',views.signup,name="signup"),
+    path('signup_page',views.page,name="page"),
+    path('registration',views.registration,name="registration"),
+    path('slist',views.studentlist.as_view(),name='s1')
 ]
--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Student
 
 def homepageview(request):
     return render(request,'home.html')
@@ -23,3 +25,31 @@ def process(request):
     print(c)
     return render(request,'ans.html',{'mya':a,'myb':b,'mysum':c})
 
+def signup(request):
+    return render(request,'signup.html')
+
+def page(request):
+    print("Done!")
+    print(request.method)
+    print(request.POST)
+    fn = str(request.POST['fname'])
+    ln = str(request.POST['lname'])
+    gn = str(request.POST['gender'])
+    dt = str(request.POST['date'])
+    ei = str(request.POST['email_id'])
+    ph = str(request.POST['phone_number'])
+    ad = str(request.POST['address'])
+    ct = str(request.POST['city'])
+    st = str(request.POST['state'])
+    zp = str(request.POST['zip'])
+    cn = str(request.POST['country'])
+
+    return render(request,'details.html',{'fname':fn, 'lname':ln, 'gender':gn, 'date':dt, 'email_id': ei,
+        'phone_number':ph, 'address':ad, 'city':ct, 'state':st, 'zip':zp, 'country':cn })
+    
+def registration(request):
+    return render(request,'registration.html')
+
+class studentlist(ListView):
+    model = Student
+    template_name = 'slist.html'
